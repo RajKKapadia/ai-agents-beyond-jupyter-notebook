b6240dc31608129c604bebd139fbf329fc54860d
RajKKapadia <raajforyou@gmail.com>
2026-02-04 12:27:42 +0530

    added input guardrail

--- src/agents/main_agent.py
+++ src/agents/main_agent.py
@@ -1,14 +1,13 @@
 from agents import Agent, OpenAIResponsesModel, AsyncOpenAI
 
 from src.config import OPENAI_MODEL, OPENAI_API_KEY
+from src.agents.guardrail import weather_guardrail
 
-agent = Agent(
-    name="Main Agent",
-    instructions="You are a helpful assistant that can help with tasks and questions.",
+weather_agent = Agent(
+    name="Weather Agent",
+    instructions="You are a helpful assistant that gets weather information for the user.",
     model=OpenAIResponsesModel(
-        model=OPENAI_MODEL,
-        openai_client=AsyncOpenAI(
-            api_key=OPENAI_API_KEY
-        )
-    )
+        model=OPENAI_MODEL, openai_client=AsyncOpenAI(api_key=OPENAI_API_KEY)
+    ),
+    input_guardrails=[weather_guardrail],
 )
